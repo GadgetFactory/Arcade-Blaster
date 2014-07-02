0017c44a888ac85614eafea2cb65281a6b18d225
Mandl <mandl@users.noreply.github.com>
2014-07-02 15:28:32 +0200

    Add papilio_prog romgen and CMake support

---
+++ Arcade-Blaster/papilio_prog/config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+//#define PACKAGE "papilio-prog"
+
+/* Define to the address where bug reports for this package should be sent. */
+//#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+//#define PACKAGE_NAME "papilio-prog"
+
+/* Define to the full name and version of this package. */
+//#define PACKAGE_STRING "papilio-prog 1.5"
+
+/* Define to the one symbol short name of this package. */
+//#define PACKAGE_TARNAME "papilio-prog"
+
+/* Define to the home page for this package. */
+//#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+//#define PACKAGE_VERSION "1.5"
+
+/* Version number of package */
+//#define VERSION "1.5"
+
+/* Cross-compiling for windows */
+/* #undef WINDOWS */
---
+++ Arcade-Blaster/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -cp papilio_arcade.jar net/gadgetfactory/papilio/arcade/PapilioArcade
+
